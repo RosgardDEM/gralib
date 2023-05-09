df17b2ea6d14614762afc68503e5514abd80a0ef
RosgardDEM <rosgarddem@gmail.com>
2023-05-10 01:12:35 +0700

    Add strapi service

--- makefile
+++ makefile
@@ -3,6 +3,9 @@ ARGS = $(shell echo $(MAKECMDGOALS) | sed 's!^.*$@!!')
 
 up:; @$(COMPOSE) up -d
 down:; @$(COMPOSE) down
+down-v:; @$(COMPOSE) down -v
 
 sh:; @$(COMPOSE) exec $(ARGS) sh
 logs:; @$(COMPOSE) logs -f $(ARGS)
+
+dev:; @$(COMPOSE) exec strapi npm run develop
---
+++ strapi/config/server.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  host: env('HOST'),
+  port: env.int('PORT'),
+  url: env('PUBLIC_URL'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
